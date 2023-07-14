b4eecb2c097660355dedebb11585ff625380944f
Mohsen Einhesari <mohseneinhesari@yahoo.com>
2023-07-14 23:37:38 +0330

    Make detailScreen column scrollable

--- feature/detail/src/main/java/com/mohsen/itollhub/detail/UserDetailScreen.kt
+++ feature/detail/src/main/java/com/mohsen/itollhub/detail/UserDetailScreen.kt
@@ -15,8 +15,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Person
@@ -65,7 +67,9 @@ fun UserDetailScreen(
         modifier = modifier.fillMaxSize(),
         topBar = { TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) }) },
     ) {
-        Column(modifier = Modifier.padding(it)) {
+        Column(modifier = Modifier
+            .padding(it)
+            .verticalScroll(rememberScrollState())) {
             viewState.user?.let {
                 UserDetailCard(it)
             } ?: run {
